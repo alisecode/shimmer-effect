56440018058b805fa8634a9eda586382cc13bebe
Alisa <alisa.melissa@icloud.com>
2024-02-13 18:30:15 +0200

    Initial Commit

---
+++ ShimmerText/ShimmerTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerTextApp.swift
+//  ShimmerText
+//
+//  Created by Алиса Третьякова on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
