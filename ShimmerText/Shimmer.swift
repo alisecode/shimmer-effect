ab0acbca4e6869c203379dc55776dfc3ae032cc7
Alisa <alisa.melissa@icloud.com>
2024-02-14 16:41:05 +0200

    commit

---
+++ ShimmerText/Shimmer.swift
@@ -0,0 +1,18 @@
+//
+//  Shimmer.swift
+//  ShimmerText
+//
+//  Created by Алиса Третьякова on 13.02.2024.
+//
+
+import SwiftUI
+
+struct Shimmer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Shimmer()
+}
